f621cca0f5cce15371ff56fba74dc3377ab452c8
Carlos <nashlasterby@gmail.com>
2022-09-03 17:47:43 -0300

    hamb-menu created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [mode, setMode] = useState(false);
+
+  function toggleMode() {
+    setMode(!mode);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        className={mode ? "icon iconActive" : "icon"}
+        onClick={toggleMode}
+      >
+        <header className="hamburguer hamburguerIcon"></header>
+      </div>
+      <nav className={mode ? 'menu menuOpen' : 'menu menuClose'}>
+        <div className="list">
+          <ul className="listItems">
+            <li>HOME</li>
+            <li>SEARCH</li>
+            <li>CART</li>
+            <li>CONTACT</li>
+            <li>ABOUT US</li>
+          </ul>
+        </div>
+      </nav>
     </div>
   );
 }
